c1c76e2048c23f4db3caf56439c83f943b3b4c5c
Aroyan <abssneakers@gmail.com>
2021-10-03 14:33:31 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementsByClassName('menu');
+
+hamburger.addEventListener('click', {
+  handleEvent: function (e) {
+    //Some Function to show up menu
+    console.log('Kusam');
+    alert('Hi there');
+  },
+});
